6e8253e1a4e160ea453b8b22f34020dd72991ae0
Joan Fisbein <joan@fisbein.com>
2018-08-14 15:53:24 +0200

    Adding support to upload to json file to git

--- src/main/java/com/sputnik/ouidb/Runner.java
+++ src/main/java/com/sputnik/ouidb/Runner.java
@@ -1,62 +1,69 @@
 package com.sputnik.ouidb;
 
 import com.sputnik.ouidb.model.Organization;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
-import java.util.Optional;
 
 public class Runner {
     private static OUIDBDownloader downloader = new OUIDBDownloader();
     private static OUIDBConverter converter = new OUIDBConverter();
     private File dataPath;
+    private String repoRemoteUri;
 
-    public Runner(File dataPath) {
+    public Runner(File dataPath, String repoRemoteUri) {
         this.dataPath = dataPath;
+        this.repoRemoteUri = repoRemoteUri;
     }
 
-    public static void main(String[] args) throws IOException {
-        if (args.length != 1) {
-            System.err.println("Expected one argument");
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Expected two argument");
         } else {
-            new Runner(new File(args[0])).run();
+            new Runner(new File(args[0]), args[1]).run();
         }
     }
 
-    private void run() throws IOException {
-        Optional<String> existingDBDigest = getExistingDBDigest();
+    private void run() throws IOException, GitAPIException {
+        Git git = getGitRepo();
 
         Map<String, Organization> parsedDB = downloader.getParsedDB();
         String json = converter.convertToJson(parsedDB);
-        String sha1 = getSha1(json.getBytes());
-        if (!existingDBDigest.orElse("").equalsIgnoreCase(sha1)) {
-            // File has changed
-            FileWriter writer = new FileWriter(getOuiDBFile());
-            IOUtils.write(json, writer);
-            writer.flush();
+        FileWriter writer = new FileWriter(getOuiDBFile());
+        IOUtils.write(json, writer);
+        writer.flush();
+        Status status = git.status().call();
+        if (!status.getModified().isEmpty()) {
             //TODO upload to git repo
+            git.add().addFilepattern(getOuiDBFile().getName()).call();
+            git.commit().setMessage("Updated OUIDB json file").call();
+            git.push().call();
         }
-    }
-
-    private String getSha1(byte[] byteArray) {
-        return DigestUtils.sha1Hex(byteArray);
-    }
-
-    private Optional<String> getExistingDBDigest() throws IOException {
-        String digest = null;
-        if (getOuiDBFile().exists()) {
-            digest = DigestUtils.sha1Hex(IOUtils.toString(new FileReader(getOuiDBFile())));
-        }
-
-        return Optional.ofNullable(digest);
     }
 
     private File getOuiDBFile() {
         return new File(dataPath, "ouidb.json");
+    }
+
+    private Git getGitRepo() throws GitAPIException, IOException {
+        Git git;
+        if (!dataPath.exists()) {
+            dataPath.mkdirs();
+        }
+        try {
+            git = Git.open(dataPath);
+            git.pull().call();
+        } catch (RepositoryNotFoundException e) {
+            git = Git.cloneRepository().setDirectory(dataPath).setURI(repoRemoteUri).call();
+        }
+
+        return git;
     }
 }
