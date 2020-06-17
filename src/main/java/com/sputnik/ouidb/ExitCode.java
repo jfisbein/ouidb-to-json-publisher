d162d3047499e6077ca6a69fbf65f36cb3bdb2e7
Joan Fisbein <joan.fisbein@clarity.ai>
2020-06-17 11:24:38 +0200

    Setting ExitCode.THERES_CHANGES to `0` to avoid github detect execution has failed.

--- src/main/java/com/sputnik/ouidb/ExitCode.java
+++ src/main/java/com/sputnik/ouidb/ExitCode.java
@@ -1,15 +1,15 @@
 package com.sputnik.ouidb;
 
 public enum ExitCode {
-    NO_CHANGES(0), THERES_CHANGES(1), PARAMS_ERROR(-1);
+  NO_CHANGES(0), THERES_CHANGES(0), PARAMS_ERROR(-1);
 
-    private final int code;
+  private final int code;
 
-    ExitCode(int code) {
-        this.code = code;
-    }
+  ExitCode(int code) {
+    this.code = code;
+  }
 
-    public int getCode() {
-        return code;
-    }
+  public int getCode() {
+    return code;
+  }
 }
